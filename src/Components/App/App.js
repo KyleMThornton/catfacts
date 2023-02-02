f413f46aba61dcb9a1af8eada09a93ba447790c1
KyleMThornton <20633426+KyleMThornton@users.noreply.github.com>
2023-02-01 21:04:26 -0800

    First commit after a bit of messing around

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import '../App/App.css'
+import '../CatFacts/CatFacts'
+import CatFacts from '../CatFacts/CatFacts';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cool Cat Facts</h1>
+        <CatFacts />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CatFacts/CatFacts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './CatFacts.css'
+
+class CatFacts extends React.Component {
+    render() {
+        return (
+            <p>This is a place holder for a cool cat fact!</p>
+        )
+    }
+}
+
+export default CatFacts;
